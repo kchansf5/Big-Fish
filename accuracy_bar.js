d94301d98d294ed61aa73877f19dc225320b9e2c
Ken <kchansf5@gmail.com>
2018-06-20 11:22:23 -0700

    Replace canvas rectangles with custom artwork

--- accuracy_bar.js
+++ accuracy_bar.js
@@ -1,59 +1,59 @@
 
-//set color constants
-
-const green = "#50ff35";
-const yellow = "#f2f241";
-const red = "#ff3434";
-
-let x = 125;
-let dx = 7;
+//initial position and speed
+let x = 25;
+let dx = 3;
 
 
 window.addEventListener('keydown', (event) => {
-  // debugger;
   let success = document.getElementById('success');
   if (event.keyCode === 32) {
+    console.log(x);
     pauseAnimation();
     success.innerText = "";
-    if (x > 160 && x < 200) {
+
+    if (x > 125 && x < 235) {
       success.innerText = "You got one!";
-      // console.  log(x);
     } else {
       success.innerText = "No fish yet.";
     }
   }
 });
 
-
 window.paused = true;
 
 const pauseAnimation = () => {
-  // debugger;
   window.paused = window.paused ? false : true;
 };
 
-const canvas = document.getElementById("bar-bg");
-canvas.width = 360;
-canvas.height = 50;
+// const canvas = document.getElementById("bar-bg");
+// canvas.width = 360;
+// canvas.height = 50;
+//
+// const c = canvas.getContext('2d');
 
-const c = canvas.getContext('2d');
 
-//accuracy bar background gradient
-const gradient = c.createLinearGradient(10,0,350,0);
-gradient.addColorStop(0, red);
-gradient.addColorStop(0.2, red);
-gradient.addColorStop(0.35, yellow);
-gradient.addColorStop(0.495, green);
-gradient.addColorStop(0.5, green);
-gradient.addColorStop(0.505, green);
-gradient.addColorStop(0.65, yellow);
-gradient.addColorStop(0.8, red);
-gradient.addColorStop(1.0, red);
+//set color constants
 
-c.fillStyle = gradient;
-c.fillRect(10, 10, 350, 30);
-c.strokeStyle="#000000";
-c.strokeRect(10, 10, 350, 30);
+// const green = "#50ff35";
+// const yellow = "#f2f241";
+// const red = "#ff3434";
+//
+// //accuracy bar background gradient
+// const gradient = c.createLinearGradient(10,0,350,0);
+// gradient.addColorStop(0, red);
+// gradient.addColorStop(0.2, red);
+// gradient.addColorStop(0.35, yellow);
+// gradient.addColorStop(0.495, green);
+// gradient.addColorStop(0.5, green);
+// gradient.addColorStop(0.505, green);
+// gradient.addColorStop(0.65, yellow);
+// gradient.addColorStop(0.8, red);
+// gradient.addColorStop(1.0, red);
+
+// c.fillStyle = gradient;
+// c.fillRect(10, 10, 350, 30);
+// c.strokeStyle="#000000";
+// c.strokeRect(10, 10, 350, 30);
 
 
 
@@ -65,17 +65,24 @@ canvas2.height = 50;
 
 const c2 = canvas2.getContext('2d');
 
+const bar = new Image();
+bar.addEventListener('load', function() {
+  c2.drawImage(bar, 25, 5, 15, 40);
+}, false);
+bar.src = "/Users/Ken/Desktop/a:A/javascript_project/big_fish/images/timingbarbar.png";
 
 function animate() {
-// debugger;
   if (!window.paused) {
     requestAnimationFrame(animate);
+
     c2.clearRect(0, 0, innerWidth, innerHeight);
-    c2.fillStyle="#f2f2f2";
-    c2.fillRect(x, 5, 10, 40);
-    c2.strokeStyle="black";
-    c2.strokeRect(x, 5, 10, 40);
-    // console.log(x);
+    c2.drawImage(bar, x, 5, 15, 40);
+    // c2.clearRect(0, 0, innerWidth, innerHeight);
+    // c2.fillStyle="url('/Users/Ken/Desktop/a:A/javascript_project/big_fish/images/timingbarbar.png')";
+    // c2.fillRect(x, 5, 10, 40);
+    // c2.strokeStyle="black";
+    // c2.strokeRect(x, 5, 1, 40);
+    // c2.bar;
     if (x > 335 || x < 25) {
       dx = -dx;
     }
@@ -86,12 +93,7 @@ function animate() {
   }
 }
 
-// document.getElementById('bar').addEventListener('click')
 animate();
-//
-//
-//
-//
 
 
 
